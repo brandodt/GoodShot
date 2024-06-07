65b0fe1f5c344fb098125dc4462f3cf73cf09cd8
Brando Dela Torre <67250900+brandodt@users.noreply.github.com>
2024-06-07 10:08:01 +0800

    feat: Add payment modal functionality to cashier index page

---
+++ public/assets/js/payment.modal.js
@@ -0,0 +1,33 @@
+// Path: public_html/public/cashier/index.php
+document.addEventListener('DOMContentLoaded', (event) => {
+    const modalLink = document.querySelector('#payment-modal-button');
+    // const cancelButton = document.querySelector('.modal-card-foot .button');
+    const closeButton = document.querySelector('#close-payment');
+    const modal = document.querySelector('#payment-modal-content');
+
+    const closeModal = () => {
+        modal.classList.remove('is-active');
+    };
+
+    const openModal = () => {
+        modal.classList.add('is-active');
+    };
+
+    modalLink.addEventListener('click', openModal);
+    // cancelButton.addEventListener('click', closeModal);
+    closeButton.addEventListener('click', closeModal);
+});
+
+function updateCashAmount(value) {
+    var cashAmountInput = document.querySelector('input[name="cashAmount"]');
+    if (cashAmountInput.value === '0') {
+        cashAmountInput.value = value;
+    } else {
+        cashAmountInput.value += value;
+    }
+}
+
+function clearCashAmount() {
+    var cashAmountInput = document.querySelector('input[name="cashAmount"]');
+    cashAmountInput.value = 0;
+}
--- public/cashier/index.php
+++ public/cashier/index.php
@@ -101,7 +101,7 @@ if (!isset($_SESSION["username"]) || $_SESSION["role"] !== "cashier") {
                                 </a>
                             </li>
                             <li class="subtitle is-5">
-                                <a class="aside-link has-text-centered" style="background: #262322; color:white;">
+                                <a id="payment-modal-button" class="aside-link has-text-centered" style="background: #262322; color:white;">
                                     <span class="icon-text">
                                         <span class="icon">
                                             <i class="fas fa-cash-register"></i>
@@ -633,16 +633,40 @@ if (!isset($_SESSION["username"]) || $_SESSION["role"] !== "cashier") {
         </div>
     </div>
 
+    <script src="../assets/js/payment.modal.js"></script>
+
     <!-- Payment Modal -->
     <div id="payment-modal-content" class="modal">
         <div class="modal-background"></div>
         <div class="modal-card">
             <header class="modal-card-head">
-                <p class="modal-card-title">Search</p>
-                <button class="delete" aria-label="close"></button>
+                <p class="modal-card-title">Settle Payment</p>
+                <button id="close-payment" class="delete" aria-label="close"></button>
             </header>
             <section class="modal-card-body">
-                <input type="number" class="input">
+                <input name="totalAmount" type="number" class="input is-large is-rounded has-text-right" value="43500" disabled>
+                <input name="cashAmount" type="number" class="input is-large is-rounded mt-2 has-text-right" value="50000" readonly>
+                <input name="changeAmount" type="number" class="input is-large is-rounded mt-2 has-text-right" value="6500" disabled>
+                <br>
+                <br>
+                <div class="columns  is-multiline">
+                    <div class="column is-one-quarter"><button class="button is-link is-fullwidth py-4" onclick="updateCashAmount(7)">7</button></div>
+                    <div class="column is-one-quarter"><button class="button is-link is-fullwidth py-4" onclick="updateCashAmount(8)">8</button></div>
+                    <div class="column is-one-quarter"><button class="button is-link is-fullwidth py-4" onclick="updateCashAmount(9)">9</button></div>
+                    <div class="column is-one-quarter"><button class="button is-danger is-light is-fullwidth py-4" onclick="clearCashAmount()">C</button></div>
+                    <div class="column is-one-quarter"><button class="button is-link is-fullwidth py-4" onclick="updateCashAmount(4)">4</button></div>
+                    <div class="column is-one-quarter"><button class="button is-link is-fullwidth py-4" onclick="updateCashAmount(5)">5</button></div>
+                    <div class="column is-one-quarter"><button class="button is-link is-fullwidth py-4" onclick="updateCashAmount(6)">6</button></div>
+                    <div class="column is-one-quarter"><button class="button is-link is-fullwidth py-4" onclick="updateCashAmount(0)">0</button></div>
+                    <div class="column is-one-quarter"><button class="button is-link is-fullwidth py-4" onclick="updateCashAmount(1)">1</button></div>
+                    <div class="column is-one-quarter"><button class="button is-link is-fullwidth py-4" onclick="updateCashAmount(2)">2</button></div>
+                    <div class="column is-one-quarter"><button class="button is-link is-fullwidth py-4" onclick="updateCashAmount(3)">3</button></div>
+                    <div class="column is-one-quarter"><button class="button is-link is-fullwidth py-4" onclick="updateCashAmount('00')">00</button></div>
+                </div>
+                <div class="columns is-centered">
+                    <div class="column is-12"><button class="button is-primary is-fullwidth">Confirm</button></div>
+                </div>
+            </section>
             </section>
             <footer class=" modal-card-foot">
             </footer>
@@ -652,7 +676,6 @@ if (!isset($_SESSION["username"]) || $_SESSION["role"] !== "cashier") {
     <!-- Scripts -->
     <script src="../assets/js/account.modal.js"></script>
     <script src="../assets/js/search.modal.js"></script>
-    <script src="../assets/js/payment.modal.js"></script>
 </body>
 
 </html>
