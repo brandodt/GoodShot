965ed5cd6729f8d6c4a2db234f2df46c33fd49c5
Brando Dela Torre <67250900+brandodt@users.noreply.github.com>
2024-06-07 11:21:45 +0800

    chore: Add login modal functionality to index page

---
+++ public/assets/js/login.modal.js
@@ -0,0 +1,19 @@
+// Path: public_html/public/index.php
+document.addEventListener('DOMContentLoaded', (event) => {
+    const forgetPasswordLink = document.querySelector('#forgetPassword');
+    const cancelButton = document.querySelector('.modal-card-foot .button');
+    const closeButton = document.querySelector('.modal-card-head .delete');
+    const modal = document.querySelector('#passwordModal');
+
+    const closeModal = () => {
+        modal.classList.remove('is-active');
+    };
+
+    const openModal = () => {
+        modal.classList.add('is-active');
+    };
+
+    forgetPasswordLink.addEventListener('click', openModal);
+    cancelButton.addEventListener('click', closeModal);
+    closeButton.addEventListener('click', closeModal);
+});
