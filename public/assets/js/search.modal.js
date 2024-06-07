759a8bc37eb31739751b9c3bc6291f958fa77f4a
Brando Dela Torre <67250900+brandodt@users.noreply.github.com>
2024-06-07 10:07:42 +0800

    Refactor search modal event listeners

--- public/assets/js/search.modal.js
+++ public/assets/js/search.modal.js
@@ -1,7 +1,7 @@
 // Path: public_html/public/cashier/index.php
 document.addEventListener('DOMContentLoaded', (event) => {
     const modalLink = document.querySelector('#search-modal-button');
-    const cancelButton = document.querySelector('.modal-card-foot .button');
+    // const cancelButton = document.querySelector('.modal-card-foot .button');
     const closeButton = document.querySelector('.modal-card-head .delete');
     const modal = document.querySelector('#search-modal-content');
 
@@ -14,6 +14,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     };
 
     modalLink.addEventListener('click', openModal);
-    cancelButton.addEventListener('click', closeModal);
+    // cancelButton.addEventListener('click', closeModal);
     closeButton.addEventListener('click', closeModal);
 });
