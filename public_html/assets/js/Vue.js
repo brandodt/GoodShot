f58fd54edcdabb36e02b8e5a5bda373065a26127
Brando Dela Torre <67250900+brandodt@users.noreply.github.com>
2024-06-09 13:54:34 +0800

    chore: Add stock and cart data to Vue.js app

--- public_html/assets/js/Vue.js
+++ public_html/assets/js/Vue.js
@@ -1,12 +1,26 @@
 Vue.createApp({
     data() {
         return {
-            searched: '',
             searchQuery: '',
             products: [],
             cart: [],
-            notification: null
+            notification: null,
+            notificationType: null,
+            cartData: {},
+            stockData: {}
         }
+    },
+    created() {
+        axios.get('../../../includes/product.php')
+            .then(response => {
+                this.products = response.data;
+                this.products.forEach(product => {
+                    this.stockData[product.id] = product.quantity;
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
     },
     methods: {
         searchProduct() {
@@ -17,6 +31,14 @@ Vue.createApp({
             })
                 .then(response => {
                     this.products = response.data;
+                    this.products.forEach(product => {
+                        if (this.stockData[product.id]) {
+                            product.quantity = this.stockData[product.id];
+                            if (this.cartData[product.id]) {
+                                product.quantity -= this.cartData[product.id];
+                            }
+                        }
+                    });
                 })
                 .catch(error => {
                     console.log(error);
@@ -24,14 +46,25 @@ Vue.createApp({
         },
         addToCart(product) {
             let quantity = prompt("Enter quantity:");
-            if (quantity > product.quantity) {
-                this.notification = "You can't add more items than available in stock.";
-            } else {
-                this.cart.push({
-                    product: product,
-                    quantity: quantity
-                });
-                this.notification = null;
+            if (quantity !== null) {
+                if (quantity > product.quantity) {
+                    this.notification = "You can't add more items than available in stock.";
+                    this.notificationType = 'danger';
+                } else {
+                    product.quantity -= quantity;
+                    this.cart.push({
+                        product: product,
+                        quantity: quantity
+                    });
+                    if (this.cartData[product.id]) {
+                        this.cartData[product.id] += quantity;
+                    } else {
+                        this.cartData[product.id] = quantity;
+                    }
+                    this.stockData[product.id] -= quantity;
+                    this.notification = `Successfully added <strong>${product.name}</strong> x <strong>${quantity}</strong>`;
+                    this.notificationType = 'success';
+                }
             }
         }
     },
