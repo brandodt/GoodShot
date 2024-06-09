e54d995b6d0af1f2be2fa3b358b2ff3c2c7a028a
Brando Dela Torre <67250900+brandodt@users.noreply.github.com>
2024-06-10 06:46:44 +0800

    chore: Initialize quantityToAdd for each product in Vue.js app

--- public_html/assets/js/Vue.js
+++ public_html/assets/js/Vue.js
@@ -13,11 +13,12 @@ Vue.createApp({
     created() {
         axios.get('../../includes/product.php')
             .then(response => {
-                this.products = response.data;
+                this.products = response.data.map(product => ({
+                    ...product,
+                    quantityToAdd: 0 // Initialize quantityToAdd for each product
+                }));
                 this.products.forEach(product => {
-                    if (!this.stockData[product.id]) {
-                        this.stockData[product.id] = product.quantity;
-                    }
+                    this.stockData[product.product_id] = product.quantity;
                 });
             })
             .catch(error => {
@@ -32,10 +33,13 @@ Vue.createApp({
                 }
             })
                 .then(response => {
-                    this.products = response.data;
+                    this.products = response.data.map(product => ({
+                        ...product,
+                        quantityToAdd: 0 // Re-initialize quantityToAdd for each product
+                    }));
                     this.products.forEach(product => {
-                        if (this.stockData[product.id]) {
-                            product.quantity = this.stockData[product.id];
+                        if (this.stockData[product.product_id] !== undefined) {
+                            product.quantity = this.stockData[product.product_id];
                         }
                     });
                 })
@@ -44,31 +48,53 @@ Vue.createApp({
                 });
         },
         addToCart(product) {
-            let quantity = prompt("Enter quantity:");
-            quantity = parseInt(quantity);
-            if (isNaN(quantity) || quantity <= 0) {
+            let quantity = product.quantityToAdd;
+            if (quantity <= 0) {
                 this.notification = "Invalid quantity. Please try again.";
                 this.notificationType = 'danger';
-            } else if (quantity !== null) {
-                if (quantity > product.quantity) {
-                    this.notification = "You can't add more items than available in stock. " + product.quantity + " item(s) left.";
-                    this.notificationType = 'danger';
+            } else if (quantity > this.stockData[product.product_id]) {
+                this.notification = "You can't add more items than available in stock. " + this.stockData[product.product_id] + " item(s) left.";
+                this.notificationType = 'danger';
+            } else {
+                const cartProduct = this.cart.find(item => item.product.product_id === product.product_id);
+                if (cartProduct) {
+                    cartProduct.quantity += quantity;
                 } else {
                     this.cart.push({
                         product: product,
                         quantity: quantity
                     });
-                    if (this.cartData[product.id]) {
-                        this.cartData[product.id] += quantity;
-                    } else {
-                        this.cartData[product.id] = quantity;
-                    }
-                    this.stockData[product.id] -= quantity;
-                    product.quantity -= quantity;
-                    this.notification = `Successfully added <strong>${product.name}</strong> x <strong>${quantity}</strong>`;
-                    this.notificationType = 'success';
                 }
+                if (this.cartData[product.product_id]) {
+                    this.cartData[product.product_id] += quantity;
+                } else {
+                    this.cartData[product.product_id] = quantity;
+                }
+                this.stockData[product.product_id] -= quantity;
+                product.quantityToAdd = 0; // Reset quantityToAdd after adding to cart
+                this.notification = `Successfully added <strong>${product.name}</strong> x <strong>${quantity}</strong>`;
+                this.notificationType = 'success';
             }
+        },
+        decreaseQuantity(item) {
+            if (item.quantity > 1) {
+                item.quantity--;
+                this.stockData[item.product.product_id]++;
+                this.cartData[item.product.product_id]--;
+            }
+        },
+        increaseQuantity(item) {
+            if (this.stockData[item.product.product_id] > 0) {
+                item.quantity++;
+                this.stockData[item.product.product_id]--;
+                this.cartData[item.product.product_id]++;
+            }
+        },
+        removeItem(index) {
+            const item = this.cart[index];
+            this.stockData[item.product.product_id] += item.quantity;
+            delete this.cartData[item.product.product_id];
+            this.cart.splice(index, 1);
         }
     },
     watch: {
