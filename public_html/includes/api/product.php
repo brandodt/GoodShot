59e46607d4cb44fc8f6d8eef394dbdc148b29def
Brando Dela Torre <67250900+brandodt@users.noreply.github.com>
2024-06-12 13:56:35 +0800

    refactor: Update product.php to include "Category" on search.modal.php

--- public_html/includes/api/product.php
+++ public_html/includes/api/product.php
@@ -8,8 +8,8 @@ $conn = $db->connect();
 // Check if 'productName' is set in the GET request, use a default value if not
 $productName = isset($_GET['productName']) ? "%" . $_GET['productName'] . "%" : "%";
 
-$stmt = $conn->prepare("SELECT * FROM products WHERE name LIKE ? OR product_id LIKE ?");
-$stmt->bind_param("ss", $productName, $productName);
+$stmt = $conn->prepare("SELECT * FROM products WHERE name LIKE ? OR product_id LIKE ? OR category LIKE ?");
+$stmt->bind_param("sss", $productName, $productName, $productName);
 $stmt->execute();
 $result = $stmt->get_result();
 $products = $result->fetch_all(MYSQLI_ASSOC);
