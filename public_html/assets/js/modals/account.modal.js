6408c266609a3743963e042fad7e854d9e4ff4a8
Brando Dela Torre <67250900+brandodt@users.noreply.github.com>
2024-06-13 10:40:36 +0800

    chore: Update directories

--- public_html/assets/js/modals/account.modal.js
+++ public_html/assets/js/modals/account.modal.js
@@ -85,7 +85,7 @@ function logout() {
     var confirmation = confirm("Are you sure you want to log out?");
     if (confirmation) {
         var xhr = new XMLHttpRequest();
-        xhr.open("POST", "../../../includes/auth/logout.php", true);
+        xhr.open("POST", "../includes/auth/logout.php", true);
         xhr.onreadystatechange = function () {
             if (this.readyState === 4 && this.status === 200) {
                 window.location.href = "../?logout=success";
