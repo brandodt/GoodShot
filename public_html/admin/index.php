5fe537a9c707bed9756462236243c32f835c8edf
Brando Dela Torre <67250900+brandodt@users.noreply.github.com>
2024-06-29 20:33:39 +0800

    refactor: Improve cashier index page with scrollable table for product sales

--- public_html/admin/index.php
+++ public_html/admin/index.php
@@ -20,7 +20,7 @@
 
         .niggus {
             display: grid;
-            height: 45vh;
+            height: 40vh;
         }
 
         .menu-list.flex-column {
@@ -36,11 +36,26 @@
         .box {
             margin: 5px;
         }
+
         .scrollable-table {
-            max-height: 30vh;
+            max-height: 35vh;
             overflow-y: auto;
-          }
+        }
+
+        .nice:hover {
+            color: #AC7DE8 !important;
+        }
+
+        canvas#orderChart,
+        canvas#ticketChart {
+            /* Make the canvas responsive */
+            width: 100% !important;
+            height: auto !important;
+            max-height: 30vh;
+            /* Prevent it from getting too tall */
+        }
     </style>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 
 <body>
@@ -50,15 +65,16 @@
                 <aside class="menu has-background-grey-light px-4 py-6">
                     <ul class="menu-list is-primary flex-column">
                         <li class="py-2"><a class="has-background-primary has-text-white">Dashboard</a></li>
-                        <li class="py-2"><a class="has-background-grey-light has-text-white" >Sales Management</a></li>
-                        <li class="py-2"><a class="has-background-grey-light has-text-white">Inventory</a></li>
-                        <li class="py-2"><a class="has-background-grey-light has-text-white">Product</a></li>
-                        <li class="py-2"><a class="has-background-grey-light has-text-white">Supplies</a></li>
-                        <li class="pt-2"><a class="has-background-grey-light has-text-white">Report</a></li>
+                        <li class="py-2"><a class="has-background-grey-light has-text-white nice">Sales Management</a>
+                        </li>
+                        <li class="py-2"><a class="has-background-grey-light has-text-white nice">Inventory</a></li>
+                        <li class="py-2"><a class="has-background-grey-light has-text-white nice">Product</a></li>
+                        <li class="py-2"><a class="has-background-grey-light has-text-white nice">Supplies</a></li>
+                        <li class="pt-2"><a class="has-background-grey-light has-text-white nice">Report</a></li>
                         <li class="flex-spacer"></li>
                         <hr>
-                        <li class="pb-2"><a class="has-background-grey-light has-text-white">Settings</a></li>
-                        <li class="py-2"><a class="has-background-grey-light has-text-white">Logout</a></li>
+                        <li class="pb-2"><a class="has-background-grey-light has-text-white nice">Settings</a></li>
+                        <li class="py-2"><a class="has-background-grey-light has-text-white nice">Logout</a></li>
                     </ul>
                 </aside>
             </div>
@@ -72,7 +88,7 @@
                                     Sales Overview
                                 </span>
                                 <section class="section">
-
+                                    <br><br>
                                     <div class="columns">
                                         <div class="column">
                                             <span class="icon-text">
@@ -122,29 +138,33 @@
                                     Purchase Overview
                                 </span>
                                 <section class="section">
-
+                                    <br>
                                     <div class="columns has-text-centered">
                                         <div class="column has-text-centered">
-                                            <span class="icon-text is-flex is-flex-direction-column is-align-items-center">
+                                            <span
+                                                class="icon-text is-flex is-flex-direction-column is-align-items-center">
                                                 <span class="icon has-text-link has-text-centered is-centered">
                                                     <i class="fas fa-2x fa-shopping-basket"></i>
                                                 </span>
-                                                <h5 class="title is-6">No. of Purchases<br><b>79</b></h5>
+                                                <h5 class="subtitle is-6">No. of Purchases<br></h5><span
+                                                    class="title is-4">79</span>
                                             </span>
                                         </div>
                                     </div>
 
                                     <div class="columns has-text-centered">
                                         <div class="column has-text-centered">
-                                            <span class="icon-text is-flex is-flex-direction-column is-align-items-center">
+                                            <span
+                                                class="icon-text is-flex is-flex-direction-column is-align-items-center">
                                                 <span class="icon has-text-success">
                                                     <i class="fas fa-2x fa-shopping-basket"></i>
                                                 </span>
-                                                <h5 class="title is-6">Purchase Amount<br><b>₱68,855</b></h5>
+                                                <h5 class="subtitle is-6">Sold Amount<br></h5><span
+                                                    class="title is-4">₱68,855</span>
                                             </span>
                                         </div>
                                     </div>
-                                    
+
                                 </section>
                             </div>
                         </div>
@@ -222,6 +242,7 @@
                                 <span class="has-text-weight-bold">
                                     Total Item Solds
                                 </span>
+                                <canvas id="orderChart"></canvas>
                             </div>
                         </div>
                         <div class="niggus cell is-col-span-2">
@@ -229,6 +250,7 @@
                                 <span class="has-text-weight-bold">
                                     Top Revenue
                                 </span>
+                                <canvas id="ticketChart"></canvas>
                             </div>
                         </div>
                     </div>
@@ -236,6 +258,57 @@
             </div>
         </div>
 
+        <script>
+            var abc = document.getElementById('orderChart').getContext('2d');
+            var orderChart = new Chart(abc, {
+                type: 'line',
+                data: {
+                    labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+                    datasets: [{
+                        label: 'Orders Shipped',
+                        data: [13, 19, 9, 13, 6, 3, 7],
+                        borderColor: "rgba(26, 100, 156, 1)",
+                        fill: false,
+                        tension: 0 // No curvature
+                    }, {
+                        label: 'Orders Placed',
+                        data: [5, 29, 5, 5, 2, 3, 10],
+                        borderColor: "rgba(71, 175, 225, 1)",
+                        fill: false,
+                        tension: 0 // No curvature
+                    }]
+                }
+            });
+
+            var def = document.getElementById("ticketChart").getContext('2d');
+            var ticketChart = new Chart(def, {
+                type: 'bar',
+                data: {
+                    labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+                    datasets: [{
+                        label: 'Important Metric',
+                        data: [169, 174, 195, 187, 140, 150, 189, 210, 175, 150, 193, 139],
+                        backgroundColor: [
+                            'rgba(26, 100, 156,0.6)',
+                            'rgba(71, 175, 225,0.4)',
+                            'rgba(26, 100, 156,0.6)',
+                            'rgba(71, 175, 225,0.4)',
+                            'rgba(26, 100, 156,0.6)',
+                            'rgba(71, 175, 225,0.4)',
+                            'rgba(26, 100, 156,0.6)',
+                            'rgba(71, 175, 225,0.4)',
+                            'rgba(26, 100, 156,0.6)',
+                            'rgba(71, 175, 225,0.4)',
+                            'rgba(26, 100, 156,0.6)',
+                            'rgba(71, 175, 225,0.4)'
+                        ]
+                    }]
+                },
+                options: {
+                    indexAxis: 'y' // Make the bars horizontal
+                }
+            });
+        </script>
 </body>
 
 </html>
