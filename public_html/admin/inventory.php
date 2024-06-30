9004a438435717d2b1f3bd79942993962ff34c97
Brando Dela Torre <67250900+brandodt@users.noreply.github.com>
2024-06-30 14:32:47 +0800

    refactor: Improve cashier index page with scrollable table for product sales

--- public_html/admin/inventory.php
+++ public_html/admin/inventory.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Dashboard</title>
+    <title>Inventory</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="assets/css/bulma/bulma.css">
     <link rel="stylesheet" href="assets/css/style.css">
@@ -15,23 +15,11 @@
             height: 20vh;
         }
 
-        .box {
-            height: 100%;
-        }
-
         .icon-text-stack {
             display: flex;
             flex-direction: column;
             align-items: center;
             text-align: center;
-        }
-
-        canvas#barChart {
-            /* Make the canvas responsive */
-            width: 100% !important;
-            height: 53vh !important;
-            max-height: 100vh;
-            /* Prevent it from getting too tall */
         }
 
         .scrollable-table {
@@ -42,36 +30,51 @@
 </head>
 
 <body>
-    <div class="fixed-grid has-7-cols">
-        <div class="grid">
-            <div class="cell nigga">
-                <aside class="menu has-background-grey-light px-4 py-6">
-                    <ul class="menu-list is-primary flex-column">
-                        <li class="py-2"><a href="index.php"
-                                class="has-background-grey-light has-text-white nice">Dashboard</a></li>
-                        <li class="py-2"><a href="sales-mgmt.php" class="has-background-grey-light has-text-white nice">Sales Management</a>
+    <div class="section">
+        <div class="columns">
+            <div class="column is-2">
+                <aside class="menu">
+                    <img src="assets/img/logo_purple.png">
+                    <p class="menu-label has-text-white">Overview</p>
+                    <ul class="menu-list">
+                        <li class="pt-2"><a href="index.php"
+                                class="has-background-grey-light has-text-white nice">Dashboard</a>
                         </li>
-                        <li class="py-2"><a href ="#" class="has-background-primary has-text-white">Inventory</a></li>
-                        <li class="py-2"><a href="product.php"
-                                class="has-background-grey-light has-text-white nice">Product</a></li>
-                        <li class="py-2"><a class="has-background-grey-light has-text-white nice">Supplies</a></li>
+                        <li class="pt-2"><a href="sales-mgmt.php"
+                                class="has-background-grey-light has-text-white nice">Sales Management</a></li>
+                    </ul>
+                    <hr>
+                    <p class="menu-label has-text-white">Storage</p>
+                    <ul class="menu-list">
+                        <li class="pt-2">
+                            <a href="#" class="has-background-primary has-text-white">Inventory</a>
+                            <ul>
+                                <li class="py-2"><a href="product.php"
+                                        class="has-background-grey-light has-text-white nice">Product</a>
+                                </li>
+                                <li class="py-2"><a class="has-background-grey-light has-text-white nice">Supplies</a>
+                                </li>
+                            </ul>
+                        </li>
                         <li class="pt-2"><a class="has-background-grey-light has-text-white nice">Report</a></li>
-                        <li class="flex-spacer"></li>
-                        <hr>
+                    </ul>
+                    <hr>
+                    <p class="menu-label has-text-white">Account</p>
+                    <ul class="menu-list">
                         <li class="pb-2"><a class="has-background-grey-light has-text-white nice">Settings</a></li>
                         <li class="py-2"><a class="has-background-grey-light has-text-white nice">Logout</a></li>
                     </ul>
                 </aside>
             </div>
-            <div class="cell is-col-span-6 pt-6">
-                <h1 class="title">Inventory Overview</h1>
+            <div class="column is-10">
+                <h1 class="title has-text-white">Inventory Overview</h1>
                 <div class="fixed-grid has-7-cols">
                     <div class="grid">
                         <div class=" cell" style="height: 20vh;">
                             <div class="icon-text-stack box">
-                                <span class="fa-stack fa-2x has-text-link">
+                                <span class="fa-stack fa-2x has-text-primary">
                                     <i class="far fa-circle fa-stack-2x"></i>
-                                    <i class="fas fas fa-shopping-basket fa-stack-1x"></i>
+                                    <i class="fas fa-coins fa-stack-1x"></i>
                                 </span>
                                 <br>
                                 <h5 class="subtitle is-6">Total Products<br><br></h5>
@@ -82,7 +85,7 @@
                             <div class="icon-text-stack box">
                                 <span class="fa-stack fa-2x has-text-primary">
                                     <i class="far fa-circle fa-stack-2x"></i>
-                                    <i class="fas fa-boxes fa-stack-1x"></i>
+                                    <i class="fas fa-chart-line fa-stack-1x"></i>
                                 </span>
                                 <br>
                                 <h5 class="subtitle is-6">Items Sold<br><br></h5>
@@ -91,9 +94,9 @@
                         </div>
                         <div class=" cell" style="height: 20vh;">
                             <div class="icon-text-stack box">
-                                <span class="fa-stack fa-2x has-text-success">
+                                <span class="fa-stack fa-2x has-text-primary">
                                     <i class="far fa-circle fa-stack-2x"></i>
-                                    <i class="fas fa-shopping-basket fa-stack-1x"></i>
+                                    <i class="fas fa-dollar-sign fa-stack-1x"></i>
                                 </span>
                                 <br>
                                 <h5 class="subtitle is-6">New Products<br><br></h5>
@@ -102,9 +105,9 @@
                         </div>
                         <div class=" cell" style="height: 20vh;">
                             <div class="icon-text-stack box">
-                                <span class="fa-stack fa-2x has-text-danger">
+                                <span class="fa-stack fa-2x has-text-primary">
                                     <i class="far fa-circle fa-stack-2x"></i>
-                                    <i class="fas fa-shopping-basket fa-stack-1x"></i>
+                                    <i class="fas fa-money-bill-wave fa-stack-1x"></i>
                                 </span>
                                 <br>
                                 <h5 class="subtitle is-6">Low Stocks<br><br></h5>
@@ -118,24 +121,24 @@
                                 </span>
                                 <br>
                                 <br>
-                                 <div class="column is-flex is-justify-content-space-between">
-                                            <span class="icon-text">
-                                                <span class="icon is-large has-text-primary">
-                                                    <i class="fas fa-2x fas fa-shopping-cart""></i>
+                                <div class="column is-flex is-justify-content-space-between">
+                                    <span class="icon-text">
+                                        <span class="icon is-large has-text-primary">
+                                            <i class="fas fa-2x fas fa-shopping-cart""></i>
                                                 </span>
-                                                <span class="title is-5">Total Category<br><b>13</b></span>
-                                            </span>
+                                                <span class=" title is-5">Total Category<br><b>13</b></span>
+                                    </span>
 
-                                            <span class="icon-text">
-                                                <span class="icon is-large has-text-primary">
-                                                    <i class="fas fa-2x fas fa-shopping-cart""></i>
+                                    <span class="icon-text">
+                                        <span class="icon is-large has-text-primary">
+                                            <i class="fas fa-2x fas fa-shopping-cart""></i>
                                                 </span>
-                                                <span class="title is-5">Total Category<br><b>13</b></span>
-                                            </span>
-                                    </div>
+                                                <span class=" title is-5">Total Category<br><b>13</b></span>
+                                    </span>
+                                </div>
                             </div>
                         </div>
-                        
+
                         <div class=" cell is-col-span-3 is-row-span-12">
                             <div class="box" style="height:100%;">
                                 <span class="has-text-weight-bold has-text-danger">
@@ -241,13 +244,13 @@
                                             <td class="has-text-weight-semibold">Tubol</td>
                                             <td class="has-text-weight-semibold">₱600</td>
                                             <td class="has-text-weight-semibold">421</td>
-                                        </tr>   
+                                        </tr>
                                     </table>
                                 </div>
                             </div>
                         </div>
-                        
-                        <div class=" cell is-col-span-2" style="height: 50vh;">
+
+                        <div class=" cell is-col-span-4" style="height: 50vh;">
                             <div class="box">
                                 <span class="has-text-weight-bold has-text-danger">
                                     Least Selling Products
@@ -319,10 +322,7 @@
             </div>
         </div>
     </div>
-    <script>
 
-       
-    </script>
 </body>
 
 </html>
